eeb6d3a8bd74425af96526e08fcbf98a3250c702
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-19 19:50:25 -0800

    fixing bugs come back later to fix state

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 
-function Calculator(){//deleted income prop
-const [budget, setBudget] = useState({ income: e.target.value, needs: 0, wants: 0, savings: 0});
+function Calculator(income){//deleted income prop
+const [budget, setBudget] = useState({ needs: 0, wants: 0, savings: 0});
 
-
-    function calculations(e){ 
-        setBudget({
+    function captureValue(e){
+        income = e.target.value
+    }
+    function Calculations({ 
+        const handleClick = () =>
+        console.log('button clicked')
+        /*setBudget({
         ...budget,
-        income: e.target.value,
-        needs: (income * .5),
-        wants: (income * .3),
-        savings: (income * .2)
-
-    })}
+        needs: (budget * .5),
+        wants: (budget * .3),
+        savings: (budget * .2)
+    */
+    }
     return (
         <>
         <label for="incomeInterval">How often do you get paid</label>
@@ -23,7 +26,8 @@ const [budget, setBudget] = useState({ income: e.target.value, needs: 0, wants: 
             <option value = "semiAnnually"> Semi-Annually </option>
             <option value = "annually"> Annually </option>
         </select>
-        <input name = "incomeInput" onChange={calculations} />
+        <input className = "incomeInput" type= "number" onChange={captureValue} />
+        <button onClick = {Calculations}>Calculate</button>
 
 
         </>
