0f34c989d85bc6bf3ec72f3c43214032d5b35d48
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-19 17:30:35 -0800

    created simple function for calculating, add to UI

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -2,10 +2,24 @@ import { useState } from 'react';
 
 
 function Calculator({income}){
+let needs = 0;
+let wants = 0;
+let savings = 0;
 
     function handleTextChange(e){
         income = e.target.value;
         console.log(income);
+    }
+
+    function calculations(e){
+        income = e.target.value;
+        needs = (income * .5)
+        console.log(needs);
+        wants = (income * .3)
+        console.log(wants);
+        savings = (income * .2)
+        console.log(savings);
+
     }
     return (
         <>
@@ -17,7 +31,7 @@ function Calculator({income}){
             <option value = "semiAnnually"> Semi-Annually </option>
             <option value = "annually"> Annually </option>
         </select>
-        <input name = "incomeInput" onChange={handleTextChange} />
+        <input name = "incomeInput" onChange={calculations} />
 
 
         </>
