bf4dad7911d9ea98855b0e3acbbe8f8ff77b6a13
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-21 09:46:18 -0800

    starting pie chart

---
+++ src/PieChart.jsx
@@ -0,0 +1,7 @@
+import Calculator from "./Calculator";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+<Doughnut data={...} />
