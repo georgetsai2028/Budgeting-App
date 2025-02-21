e60250189bae3b6f09b0090acd5952732931ccfa
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-21 08:46:43 -0800

    trying to make ui work

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -5,17 +5,19 @@ const [budget, setBudget] = useState({ needs: 0, wants: 0, savings: 0});
 
     function captureValue(e){
         income = e.target.value
-    }
-    function Calculations({ 
-        const handleClick = () =>
-        console.log('button clicked')
-        /*setBudget({
+        
+    };
+
+    function Calculations(){ 
+        setBudget({
         ...budget,
         needs: (budget * .5),
         wants: (budget * .3),
         savings: (budget * .2)
-    */
-    }
+    });
+   }
+    
+
     return (
         <>
         <label for="incomeInterval">How often do you get paid</label>
@@ -26,7 +28,7 @@ const [budget, setBudget] = useState({ needs: 0, wants: 0, savings: 0});
             <option value = "semiAnnually"> Semi-Annually </option>
             <option value = "annually"> Annually </option>
         </select>
-        <input className = "incomeInput" type= "number" onChange={captureValue} />
+        <input value = {income} type= "number" onChange={captureValue} />
         <button onClick = {Calculations}>Calculate</button>
 
 
