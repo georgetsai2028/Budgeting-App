cceb95917eaea5e93d9a6ab82ae381d14a465810
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-22 15:08:15 -0800

    fixed display issue, clean up to make look pretty

--- src/PieChart.jsx
+++ src/PieChart.jsx
@@ -5,20 +5,20 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 export function PieChart({budget}) {
  const data = {
-  labels: Object.keys(budget),
+  labels: ["Needs", "Wants", "Savings"],
   datasets: [
     {
       data: Object.values(budget),
-      label: ["Needs", "Wants", "Savings"],
+
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
+        'rgba(0, 187, 31, 0.2)',
       ],
       borderColor: [
         'rgba(255, 99, 132, 1)',
         'rgba(54, 162, 235, 1)',
-        'rgba(75, 192, 192, 1)',
+        'rgb(8, 168, 40)',
       ],
       borderWidth: 1,
     },
