4b2f3ea3f7372c0511d42e14e7a0239ed1734bdf
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-23 12:38:38 -0800

    created basic route for user dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import {Calculator} from './Calculator.jsx'
+import UserDashboard from './pages/UserDashboard.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"./pages/UserDashboard.jsx",
+    element: <UserDashboard />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,9 +1,14 @@
 
+import { Outlet } from "react-router-dom";
+
 const UserDashboard = () => {
+
     return (
 
         <>
         <h1>HELLO </h1>
+        <h2> Here is the main page: </h2>
+        <Outlet />
         </>
     );
     
