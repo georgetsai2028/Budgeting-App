5de206829cb3d8ce32cae589e2fc6681593786eb
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-27 19:03:38 -0800

    fixed bugs need to readd user dashboard

---
+++ src/pages/UserDashboard.jsx
@@ -0,0 +1,23 @@
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './UserDashboard.css'
+
+
+export function UserDashboard() {
+
+
+    return (
+        <>
+        </>
+    );
+    
+};
+
+createRoot(document.getElementById('root')).render(
+<StrictMode>
+    <UserDashboard />
+</StrictMode>
+)
+
