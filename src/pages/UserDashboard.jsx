c6792c3978c0a6da18cf8c461a5cf71c1cf2dd72
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-27 20:06:11 -0800

    creating grid

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -9,11 +9,21 @@ export function UserDashboard() {
 
 
     return (
-        <div className="mainContainer">
+        <>
+            <div className="mainContainer">
+            <header>
+                Hey User!
+            </header>
             <div className="gridContainer">
-                
+                <div id="purchaseTracker">1</div>
+                <div id="piggybank">2</div>
+                <div id="investments">3</div>
+                <div id="barChart">4</div>
+                <div id="score">5</div>
             </div>
-        </div>
+            </div>
+        </>
+        
     );
     
 };
