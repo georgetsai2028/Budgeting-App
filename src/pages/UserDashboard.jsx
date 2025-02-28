4f1d2e41415f33d4a0906b9f5be000ca0fc35339
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-02-27 18:55:05 -0800

    added css changes for userdashboard

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,16 +1,26 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './UserDashboard.css'
 
 
 export function UserDashboard() {
 
     return (
-
         <>
         <h1>HELLO </h1>
         <h2> Here is the main page: </h2>
         </>
+        
     );
     
 };
+
+createRoot(document.getElementById('root')).render(
+<StrictMode>
+    <UserDashboard />
+</StrictMode>
+)
 
 
 
