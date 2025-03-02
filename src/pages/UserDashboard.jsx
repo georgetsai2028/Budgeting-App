4537646b01affa54250efed257cb44053846c66a
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-03-01 20:50:11 -0800

    added names to boxes to know which is what

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -16,11 +16,11 @@ export function UserDashboard() {
                 Hey User!
             </header>
             <div className="gridContainer">
-                <div className = "item" id="purchaseTracker">1</div>
-                <div className = "item" id="piggybank">2</div>
-                <div className = "item" id="investments">3</div>
-                <div className = "item" id="barChart">4</div>
-                <div className = "item" id="score">5</div>
+                <div className = "item" id="purchaseTracker">Track your purchases!</div>
+                <div className = "item" id="piggybank">Piggybank</div>
+                <div className = "item" id="investments">Investments</div>
+                <div className = "item" id="barChart">Bar Chart</div>
+                <div className = "item" id="score">Monthly Score</div>
             </div>
             </div>
             </div>
