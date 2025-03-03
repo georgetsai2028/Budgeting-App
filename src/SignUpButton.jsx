3bed7e04a83f817bddd8710465e07026682b20b4
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-03-02 21:23:43 -0800

    changed navigation for button

--- src/SignUpButton.jsx
+++ src/SignUpButton.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 export function SignUpButton() {
     const navigate = useNavigate();
     const handleButtonClick = () => {
-        navigate("/user-dashboard")
+        navigate("/sign-up")
             
        
     }
