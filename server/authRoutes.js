4932853f9460ee69227fd7839eb9547ee1704b09
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-03-08 13:23:00 -0800

    created router post for registration and login

--- server/authRoutes.js
+++ server/authRoutes.js
@@ -4,3 +4,38 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //to save new user to database
+    const newUser = new User({username, password: hashedPassword});
+    await newUser.save();
+
+    res.status(201).send('User registered');
+});
+
+//User login route (verifies password and generates JWT)
+router.post('/login', async(req, res) => {
+    const { username, password } = req.body;
+
+    // Find the user from the database
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(401).send('Invalid Credentials');
+
+    }
+
+    //compare password with hashed password store in database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+        return res.status(401).send('Invalid Credentials');
+    }
+
+    //Generate JWT Token
+    const token = jwt.sign({userId: user._id}, 'jwt_secret', {expiresIn: '1h'});
+
+    res.json({ token });
+});
