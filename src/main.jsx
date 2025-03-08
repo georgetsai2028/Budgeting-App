94e3659eaba8a3e6c5ec43af527f6969330e0d2c
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-03-07 23:29:53 -0800

    api fetching works

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import axios from "axios"
 import {Calculator} from './Calculator.jsx'
 import { UserDashboard } from './pages/UserDashboard.jsx'
 import { SignUp } from './pages/SignUp.jsx'
 
 export default function App() {
+  //waits for axios to fetch array containing fruits and stores in res variable
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    console.log(response.data.fruits);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
   return (
     <BrowserRouter>
     <Routes>
