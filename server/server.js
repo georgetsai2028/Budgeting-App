8a54a452470125f3f0e6b2ca437e66d09befca27
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-03-07 23:24:05 -0800

    added specific cors and axios for fetching

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const corsOptions = {
+    origin: "http://localhost:5173",
+};
+
+app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
     res.json({ fruits: ["apple", "oranges", "pears"]});
